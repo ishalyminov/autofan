299392a177f7073469fe41698571893993fe5962
Igor Shalyminov <ishalyminov@gmail.com>
2017-09-13 01:06:18 +0000

    no pandas

--- play.py
+++ play.py
@@ -3,7 +3,6 @@ from os import path
 import logging
 import argparse
 from inspect import ismodule, getmembers
-import pandas as pd
 
 import time
 from selenium import webdriver
@@ -15,25 +14,27 @@ import apps
 logging.basicConfig(level=logging.INFO)
 
 APPS = filter(lambda member: ismodule(member[1]), getmembers(apps))
-DISPLAY = Display(visible=1, size=(1280, 720))
-PROXIES_TABLE = pd.read_csv('proxy_list.csv', delimiter=';')
+DISPLAY = Display(visible=0, size=(800, 600))
+
+with open('proxy_list.txt') as proxy_in:
+    PROXIES_LIST = [line.strip().split(':') for line in proxy_in.readlines()]
 
 def play_url(in_url):
-    proxy = PROXIES_TABLE.sample(1)
-    proxy_host = proxy.sample(1).iloc[0]['host']
-    proxy_port = proxy.sample(1).iloc[0]['port']
+    proxy_host, proxy_port = random.choice(PROXIES_LIST)
     fp = webdriver.FirefoxProfile()
     fp.set_preference('media.volume_scale', '0.0')
     fp.set_preference("network.proxy.type", 1)
     fp.set_preference("network.proxy.ssl", proxy_host)
-    fp.set_preference("network.proxy.ssl_port", proxy_port)
+    fp.set_preference("network.proxy.ssl_port", int(proxy_port))
     fp.set_preference("network.proxy.http", proxy_host)
-    fp.set_preference("network.proxy.http_port", proxy_port)
+    fp.set_preference("network.proxy.http_port", int(proxy_port))
     try:
+        driver = None
         driver = webdriver.Firefox(
             executable_path=path.join(path.dirname(path.abspath(__file__)), 'geckodriver'),
             firefox_profile=fp
         )
+        import pdb; pdb.set_trace()
         for app_name, app in APPS:
             if app.URL_PATTERN in in_url:
                 app.play(in_url, driver)
@@ -41,7 +42,8 @@ def play_url(in_url):
     except Exception as e:
         logging.error('Exception using proxy {}:{}'.format(proxy_host, proxy_port))
     finally:
-        driver.quit()
+        if driver is not None:
+            driver.quit()
     
 
 
