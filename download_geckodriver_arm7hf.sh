8b05450aa235d2cf17e3c2f5399165b47052203f
Igor Shalyminov <ishalyminov@gmail.com>
2017-09-14 01:56:22 +0100

    PhantomJS instead of gecko

--- download_geckodriver_arm7hf.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-arm7hf.tar.gz 
-tar -xvzf geckodriver-v0.18.0-arm7hf.tar.gz 
-rm geckodriver-v0.18.0-arm7hf.tar.gz 
