78967ac27427412789a59a9cc16b0b3d6a68336e
Igor Shalyminov <ishalyminov@gmail.com>
2017-09-22 01:30:39 +0100

    fix to heroku config

--- heroku_init.sh
+++ heroku_init.sh
@@ -1,4 +1,4 @@
-heroku create --stack cedar --buildpack https://github.com/heroku/heroku-buildpack-chromedriver.git
+heroku create --stack cedar-14 --buildpack https://github.com/heroku/heroku-buildpack-chromedriver.git
 heroku buildpacks:add heroku/python
 heroku buildpacks:add https://github.com/heroku/heroku-buildpack-xvfb-google-chrome
 
